b88abfb94bf2bbff19e4df4fc1c33aa61df90344
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-04-20 16:39:59 -0400

    update .env reference

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,4 @@ import "@testing-library/react/cleanup-after-each";
 import "@testing-library/jest-dom/extend-expect";
 
 import dotenv from "dotenv";
-dotenv.config({ path: './".env.development"' });
+dotenv.config({ path: './".env.test"' });
